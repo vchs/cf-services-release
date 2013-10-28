6ec61e7d15208ec3931f770ad7e1300ae210a11f
Chunjie Zhu <czhu@vmware.com>
2013-10-28 14:55:28 +0800

    add mssql provisioner and releated spec:unit
    Change-Id: Ia38512609e279277f0a0afedfbc0d555ef892632

---
+++ src/mssql_service/spec/spec_helper.rb
@@ -0,0 +1,35 @@
+# Copyright (c) 2009-2011 VMware, Inc.
+$:.unshift File.join(File.dirname(__FILE__), '..')
+$:.unshift File.join(File.dirname(__FILE__), '..', 'lib')
+
+def require_dir(dir_pattern)
+  Dir.glob(File.expand_path(dir_pattern, File.dirname(__FILE__))) do |filename|
+    require filename
+  end
+end
+
+require_dir '../vendor/integration-test-support/support/**/*.rb'
+require_dir 'support/**/*.rb'
+
+tmp_dir = File.expand_path('../tmp', File.dirname(__FILE__))
+FileUtils.mkdir_p(tmp_dir)
+IntegrationExampleGroup.tmp_dir = tmp_dir
+
+RSpec.configure do |config|
+  config.include IntegrationExampleGroup, type: :integration, :example_group => {:file_path => /\/integration\//}
+end
+
+require "rubygems"
+require "rspec"
+require "bundler/setup"
+require "vcap_services_base"
+
+require_relative "../lib/mssql_service/common"
+require_relative "../lib/mssql_service/provisioner"
+
+def getLogger()
+  logger = Logger.new(STDOUT)
+  logger.level = Logger::DEBUG
+
+  logger
+end
---
+++ src/mssql_service/spec/unit/provisioner_spec.rb
@@ -0,0 +1,56 @@
+require_relative "../spec_helper"
+
+describe VCAP::Services::MSSQL::Provisioner do
+
+  before do
+    described_class.any_instance.stub(:initialize)
+    subject.instance_variable_set(:@logger, getLogger)
+  end
+
+  describe "#generate_recipes" do
+    context "when single node topology" do
+      it "should generates a valid recipe" do
+        service_id = subject.generate_service_id
+        node_id = "mssql_node_free_1"
+        version = "MSSQLSERVER2008R2"
+        host = "192.168.56.100"
+        best_nodes = [{
+          "id" => node_id,
+          "host" => host,
+          "port" => 9999
+        }]
+        plan_config = {:free => {:lowwater => 10}}
+
+        recipes = subject.generate_recipes(service_id, plan_config , version, best_nodes)
+
+        config = recipes["configuration"]
+        config.should be_instance_of Hash
+        config["version"].should eq version
+        config["plan"].should eq "free"
+
+        credentials = recipes["credentials"]
+        credentials.should be_instance_of Hash
+        credentials["peers"].should be_nil
+
+        peers = config["peers"]
+        peers.should be_instance_of Hash
+
+        credentials = recipes["credentials"]
+        credentials.should be peers["active"]["credentials"]
+        credentials["name"].should eq service_id
+        credentials["node_id"].should eq node_id
+        credentials["port"].should eq 9999
+      end
+    end
+  end
+
+  describe "#get_port" do
+    it "should be get default port for MSSQL" do
+      subject.get_port.should eq 1433
+    end
+
+    it "should be get port for MSSQL" do
+      subject.get_port(9999).should eq 9999
+    end
+  end
+end
