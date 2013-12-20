89516ddec7ef79ebaae2585a96db3fc09575173b
khalilz <kandmforever@126.com>
2013-12-20 18:16:11 +0800

    add restore task for MSSQL
    Change-Id: I410533dcfcd11e00064742b6e12df6c7bf5acd6b

--- src/mssql_service/lib/mssql_service/provisioner.rb
+++ src/mssql_service/lib/mssql_service/provisioner.rb
@@ -33,37 +33,60 @@ class VCAP::Services::MSSQL::Provisioner < VCAP::Services::Base::Provisioner
 
   # Called by MSSQL ResourceManager which handle the http request coms from SC
   def create_backup(service_id, backup_id, opts = {}, &blk)
-    @logger.debug("Create backup task for service_id=#{service_id}")
-
-    @logger.debug(opts)
+    @logger.debug("Backup task for service_id=#{service_id}")
+    @logger.debug("Backup task opts: #{opts}")
 
     options = {
-                :id         => generate_credential,
-                :name       => "backup",
-                :node_id    => find_backup_peer(service_id), # used to identify queue "#{queue_name}:q:#{node_id}"
-                :service_id => service_id,
-                :backup_id  => backup_id,
-                :properties => opts
-              }
+      :id         => generate_credential,
+      :name       => "backup",
+      :node_id    => find_backup_peer(service_id), # used to identify queue "#{queue_name}:q:#{node_id}"
+      :service_id => service_id,
+      :backup_id  => backup_id,
+      :properties => opts # user_triggered_options({:service_id => xxx, :backup_id => xxx, :update_url => xxx, ...})
+    }
 
     BackupTask.create(options)
 
-    @logger.info("BackupJob created: #{options}")
+    @logger.info("BackupTask created: #{options}")
     blk.call(success)
   rescue => e
-    @logger.warn("BackupJob failed: #{e}")
+    @logger.warn("BackupTask failed: #{e}")
     @logger.warn(e)
     blk.call(failure(e))
   end
 
-  def user_triggered_options(args)
-    args.merge({ :type => args["type"] || "full", :trigger_by => "user" })
+  # Called by base provisioner(provisioner_v3.rb), and we should overried it here
+  def restore_backup(service_id, backup_id, node_id, ori_service_id, opts)
+   @logger.debug("Restore task for service_id=#{service_id}")
+   @logger.debug("Restore task opts: #{opts}")
+
+   # as for MSSQL restoring and it needs the credential from VCAP::Services::Internal::ServiceRecipes
+   recipes = opts[:recipes]
+   peer = recipes.configuration["peers"].find { |peer| peer["credentials"]["node_id"] == node_id }
+
+   options = {
+     :id                     => generate_credential,
+     :name                   => "restore",
+     :node_id                => node_id, # used to identify queue "#{queue_name}:q:#{node_id}"
+     :service_id             => service_id,
+     :original_database_name => find_credential_name(ori_service_id),
+     :original_service_id    => ori_service_id,
+     :backup_id              => backup_id,
+     :credential             => peer["credentials"]
+   }
+
+   # Handle restore response from Node through NATS(MSSQL.restore_backup.service_id) in the provisioner_v3.rb
+   RestoreTask.create(options)
+
+   @logger.info("RestoreTask created: #{options}")
+   true
+  rescue => e
+   @logger.warn("RestoreTask failed: #{e}")
+   @logger.warn(e)
+   false
   end
 
-  def periodically_triggered_options(args)
-  end
-
-  # Receive backup response from Node throug NATS: MSSQL.backup
+  # Handle backup response from Node throug NATS(MSSQL.backup)
   def on_backup(msg, reply)
     @logger.debug("Receive backup task response: #{msg}")
     rep = BackupTaskResponse.decode(msg)
@@ -90,7 +113,29 @@ class VCAP::Services::MSSQL::Provisioner < VCAP::Services::Base::Provisioner
     @node_nats.publish(reply, simple_rep.encode)
   end
 
-  def generate_recipes(service_id, plan_config, version, best_nodes)
+  # Generate properties for Restoring and Provisioning
+  def gen_provision_properties(service_id)
+    status = is_restoring?(service_id) ? "Restoring" : "Provisioning"
+    { :status => status }
+  end
+
+  def user_triggered_options(args)
+    args.merge({ :type => args[:type] || "full", :trigger_by => "user" })
+  end
+
+  def periodically_triggered_options(args)
+  end
+
+  def find_credential_name(service_id)
+    svc = get_instance_handle(service_id)
+    raise ServiceError.new(ServiceError::NOT_FOUND, "service id #{service_id}") if svc.nil?
+    name = svc[:credentials]["name"]
+    raise "Cannot find name for #{service_id}" if name.nil?
+    name
+  end
+  
+  # Generate MSSQL recipes for both single node(peer) and multiple peers topology.
+  def generate_recipes(service_id, plan_config, version, best_nodes, original_credential=nil)
     credentials = {}
     configuration = {
       "version" => version,
@@ -98,9 +143,14 @@ class VCAP::Services::MSSQL::Provisioner < VCAP::Services::Base::Provisioner
     }
     peers_config = []
     # Must not prefix with number
-    name = 'd' + generate_credential(dbname_length)
-    user = 'u' + generate_credential(dbname_length)
-    password = 'p' + generate_credential(password_length)
+    name = "d" + generate_credential(dbname_length)
+    user = "u" + generate_credential(dbname_length)
+    password = "p" + generate_credential(password_length)
+
+    # If it's restoring, and we still generate different database name on the fly
+    if original_credential && original_credential.has_key?("user") && original_credential.has_key?("password")
+      user, password = %w(user password).map { |key| original_credential[key] }
+    end
 
     # configure active node
     active_node = best_nodes.shift
@@ -144,6 +194,7 @@ class VCAP::Services::MSSQL::Provisioner < VCAP::Services::Base::Provisioner
     configuration["peers"] = peers_config
     credentials["peers"] = peers_config if best_nodes.size > 1
     configuration["backup_peer"] = get_backup_peer(credentials)
+    configuration["properties"] = gen_provision_properties(service_id)
 
     recipes = VCAP::Services::Internal::ServiceRecipes.new
     recipes.credentials = credentials
@@ -235,3 +286,4 @@ end
 # Alias
 MessageQueue = VCAP::Services::MSSQL::MessageQueue
 BackupTask = VCAP::Services::MSSQL::BackupTask
+RestoreTask = VCAP::Services::MSSQL::RestoreTask
