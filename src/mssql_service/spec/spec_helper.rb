586556577df458311a4172f34715283d7324bc7b
khalilz <kandmforever@126.com>
2013-12-20 18:16:11 +0800

    add restore task for MSSQL
    Change-Id: I410533dcfcd11e00064742b6e12df6c7bf5acd6b

--- src/mssql_service/spec/spec_helper.rb
+++ src/mssql_service/spec/spec_helper.rb
@@ -8,16 +8,16 @@ def require_dir(dir_pattern)
   end
 end
 
-require_dir '../vendor/integration-test-support/support/**/*.rb'
-require_dir 'support/**/*.rb'
+#require_dir '../vendor/integration-test-support/support/**/*.rb'
+#require_dir 'support/**/*.rb'
 
-tmp_dir = File.expand_path('../tmp', File.dirname(__FILE__))
-FileUtils.mkdir_p(tmp_dir)
-IntegrationExampleGroup.tmp_dir = tmp_dir
+#tmp_dir = File.expand_path('../tmp', File.dirname(__FILE__))
+#FileUtils.mkdir_p(tmp_dir)
+#IntegrationExampleGroup.tmp_dir = tmp_dir
 
-RSpec.configure do |config|
-  config.include IntegrationExampleGroup, type: :integration, :example_group => {:file_path => /\/integration\//}
-end
+#RSpec.configure do |config|
+#  config.include IntegrationExampleGroup, type: :integration, :example_group => {:file_path => /\/integration\//}
+#end
 
 require "rubygems"
 require "rspec"
